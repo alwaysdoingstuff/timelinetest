879d23e2db6660855cfd174cbc9340fceabca0b1
alwaysdoingstuff <github@preventmail.com>
2017-12-21 22:30:44 -0600

    Update ClientVersion

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -12,7 +12,7 @@
  * for both bitcoind and bitcoin-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("MV#EVTAT363421791");
 
 /**
  * Client version number
@@ -41,11 +41,10 @@ const std::string CLIENT_NAME("Satoshi");
 #include <obj/build.h>
 #endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
-#define GIT_ARCHIVE 1
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "18a1bbad98bd"
-#define GIT_COMMIT_DATE "Tue, 19 Dec 2017 15:16:33 -0500"
+#define GIT_COMMIT_ID "$Format:%h$"
+#define GIT_COMMIT_DATE "$Format:%cD$"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
@@ -101,3 +100,4 @@ std::string FormatSubVersion(const std::string& name, int nClientVersion, const 
     ss << "/";
     return ss.str();
 }
+
