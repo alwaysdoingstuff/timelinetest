e5d86cf4f5f22535507d70d98a95a2b387c2edbe
Timeline Devs <development@timelinehub.org>
2017-12-23 18:14:00 -0600

    Change Token Unit Name

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -17,9 +17,9 @@ BitcoinUnits::BitcoinUnits(QObject *parent):
 QList<BitcoinUnits::Unit> BitcoinUnits::availableUnits()
 {
     QList<BitcoinUnits::Unit> unitlist;
-    unitlist.append(BTC);
-    unitlist.append(mBTC);
-    unitlist.append(uBTC);
+    unitlist.append(TIMELINE);
+    unitlist.append(mTIMELINE);
+    unitlist.append(uTIMELINE);
     return unitlist;
 }
 
@@ -27,9 +27,9 @@ bool BitcoinUnits::valid(int unit)
 {
     switch(unit)
     {
-    case BTC:
-    case mBTC:
-    case uBTC:
+    case TIMELINE:
+    case mTIMELINE:
+    case uTIMELINE:
         return true;
     default:
         return false;
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("μBTC");
+    case TIMELINE: return QString("TIMELINE");
+    case mTIMELINE: return QString("mTIMELINE");
+    case uTIMELINE: return QString::fromUtf8("μTIMELINE");
     default: return QString("???");
     }
 }
@@ -51,9 +51,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Bitcoins");
-    case mBTC: return QString("Milli-Bitcoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Bitcoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case TIMELINE: return QString("Bitcoins");
+    case mTIMELINE: return QString("Milli-Bitcoins (1 / 1" THIN_SP_UTF8 "000)");
+    case uTIMELINE: return QString("Micro-Bitcoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
@@ -62,9 +62,9 @@ qint64 BitcoinUnits::factor(int unit)
 {
     switch(unit)
     {
-    case BTC:  return 100000000;
-    case mBTC: return 100000;
-    case uBTC: return 100;
+    case TIMELINE:  return 100000000;
+    case mTIMELINE: return 100000;
+    case uTIMELINE: return 100;
     default:   return 100000000;
     }
 }
@@ -73,9 +73,9 @@ int BitcoinUnits::decimals(int unit)
 {
     switch(unit)
     {
-    case BTC: return 8;
-    case mBTC: return 5;
-    case uBTC: return 2;
+    case TIMELINE: return 8;
+    case mTIMELINE: return 5;
+    case uTIMELINE: return 2;
     default: return 0;
     }
 }
--- src/qt/bitcoinunits.h
+++ src/qt/bitcoinunits.h
@@ -56,9 +56,9 @@ public:
      */
     enum Unit
     {
-        BTC,
-        mBTC,
-        uBTC
+        TIMELINE,
+        mTIMELINE,
+        uTIMELINE
     };
 
     enum SeparatorStyle
